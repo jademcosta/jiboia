55ff3a745c134882c06bffacaf694ffc576cc633
Jade Moreira da Costa <jademcosta@gmail.com>
2023-08-07 14:36:54 -0300

    Call sync after each localstorage write (#58)
    * Call sync after each localstorage write
    
    * Check file syncing error

--- pkg/adapters/objstorage/localstorage/localstorage.go
+++ pkg/adapters/objstorage/localstorage/localstorage.go
@@ -60,9 +60,20 @@ func (storage *LocalStorage) Upload(workU *domain.WorkUnit) (*domain.UploadResul
 
 	fullFilePath := filepath.Join(storage.path, workU.Prefix, workU.Filename)
 
-	err = os.WriteFile(fullFilePath, workU.Data, os.ModePerm)
+	file, err := os.Create(fullFilePath)
+	if err != nil {
+		return nil, fmt.Errorf("error creating file: %w", err)
+	}
+	defer file.Close()
+
+	_, err = file.Write(workU.Data)
 	if err != nil {
 		return nil, fmt.Errorf("error writing data into file: %w", err)
+	}
+
+	err = file.Sync()
+	if err != nil {
+		return nil, fmt.Errorf("error when syncing file: %w", err)
 	}
 
 	return &domain.UploadResult{
