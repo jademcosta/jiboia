566276005a681dda4d94b31c3c8e0b412fc18223
jademcosta <jademcosta@gmail.com>
2023-08-07 14:28:25 -0300

    Call sync after each localstorage write

--- pkg/adapters/objstorage/localstorage/localstorage.go
+++ pkg/adapters/objstorage/localstorage/localstorage.go
@@ -60,10 +60,18 @@ func (storage *LocalStorage) Upload(workU *domain.WorkUnit) (*domain.UploadResul
 
 	fullFilePath := filepath.Join(storage.path, workU.Prefix, workU.Filename)
 
-	err = os.WriteFile(fullFilePath, workU.Data, os.ModePerm)
+	file, err := os.Create(fullFilePath)
+	if err != nil {
+		return nil, fmt.Errorf("error creating file: %w", err)
+	}
+	defer file.Close()
+
+	_, err = file.Write(workU.Data)
+	// err = os.WriteFile(fullFilePath, workU.Data, os.ModePerm)
 	if err != nil {
 		return nil, fmt.Errorf("error writing data into file: %w", err)
 	}
+	file.Sync()
 
 	return &domain.UploadResult{
 		Bucket:      "localstorage",
