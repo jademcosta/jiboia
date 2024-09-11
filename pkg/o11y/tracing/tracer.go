c8f24a838fc7feefd409c23f63b5b57245ac7841
jademcosta <jademcosta@gmail.com>
2024-09-11 08:50:48 -0300

    Add tracer basics

---
+++ pkg/o11y/tracing/tracer.go
@@ -0,0 +1,60 @@
+package tracing
+
+import (
+	"context"
+
+	"github.com/jademcosta/jiboia/pkg/config"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdk "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.26.0"
+	"go.opentelemetry.io/otel/trace"
+	"go.opentelemetry.io/otel/trace/noop"
+)
+
+func NewNoopTracer() trace.Tracer {
+	return noop.NewTracerProvider().Tracer("github.com/jademcosta/jiboia")
+}
+
+func NewTracer(conf config.Config) (trace.Tracer, func(context.Context) error) {
+	bsp := sdk.NewBatchSpanProcessor(newExporter())
+	tracerProvider := sdk.NewTracerProvider(
+		sdk.WithSampler(sdk.AlwaysSample()),
+		sdk.WithResource(buildResource()),
+		sdk.WithSpanProcessor(bsp),
+	)
+
+	otel.SetTracerProvider(tracerProvider)
+	otel.SetTextMapPropagator(propagation.TraceContext{})
+	tracer := tracerProvider.Tracer("github.com/jademcosta/jiboia")
+
+	return tracer, tracerProvider.Shutdown
+}
+
+func newExporter() sdk.SpanExporter {
+	ctx := context.Background() //TODO: use a real one
+	exporter, err := otlptracehttp.New(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	return exporter
+}
+
+func buildResource() *resource.Resource {
+	res, err := resource.Merge(
+		resource.Default(),
+		resource.NewWithAttributes(
+			semconv.SchemaURL,
+			semconv.ServiceName("jiboia"),
+		),
+	)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return res
+}
